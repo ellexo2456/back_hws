28eb32cabd5ce0b2d142c3c41810f4564b71f839
Alexey <ax.chinaev@yandex.ru>
2023-09-07 21:13:58 +0300

    change uniq func implementation

--- unique/unqiue.go
+++ unique/unqiue.go
@@ -1,9 +1,12 @@
 package unique
 
-import (
-	"slices"
-)
-
 func Unique(lines []string) []string {
-	return slices.Compact(lines)
+	var unique_lines []string
+	unique_lines = append(unique_lines, lines[0])
+	for i, line := range lines[1:] {
+		if line != lines[i] {
+			unique_lines = append(unique_lines, line)
+		}
+	}
+	return unique_lines
 }
