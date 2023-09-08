19c2bf891b3fa97ac3d7571a5f91106265757d75
Alexey <ax.chinaev@yandex.ru>
2023-09-08 09:50:54 +0300

    add d and u flags

--- unique/unqiue.go
+++ unique/unqiue.go
@@ -4,6 +4,8 @@ import "strconv"
 
 type Options struct {
 	C *bool
+	D *bool
+	U *bool
 }
 
 func Unique(lines []string, options Options) []string {
@@ -15,17 +17,35 @@ func Unique(lines []string, options Options) []string {
 		if line != lines[i] {
 			if *options.C {
 				uniqueLines[len(uniqueLines)-1] = strconv.Itoa(count) + " " + uniqueLines[len(uniqueLines)-1]
-				count = 1
+			} else if *options.D {
+				if count == 1 {
+					uniqueLines = uniqueLines[:len(uniqueLines)-1]
+				}
+			} else if *options.U {
+				if count > 1 {
+					uniqueLines = uniqueLines[:len(uniqueLines)-1]
+				}
 			}
 
 			uniqueLines = append(uniqueLines, line)
+			count = 1
+
 			continue
 		}
 
 		count++
 	}
+
 	if *options.C {
 		uniqueLines[len(uniqueLines)-1] = strconv.Itoa(count) + " " + uniqueLines[len(uniqueLines)-1]
+	} else if *options.D {
+		if count == 1 {
+			uniqueLines = uniqueLines[:len(uniqueLines)-1]
+		}
+	} else if *options.U {
+		if count > 1 {
+			uniqueLines = uniqueLines[:len(uniqueLines)-1]
+		}
 	}
 
 	return uniqueLines
