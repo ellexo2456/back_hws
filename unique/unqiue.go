a46330a7d96c02257dfc2fe90816343b031439ec
Alexey <ax.chinaev@yandex.ru>
2023-09-08 14:24:37 +0300

    add f option

--- unique/unqiue.go
+++ unique/unqiue.go
@@ -23,7 +23,29 @@ func Unique(lines []string, options Options) []string {
 	count := 1
 	//strings.Join(strings.Fields(lines[0])[1:], " ")
 	for i, line := range lines[1:] {
-		if line != lines[i] && !*options.I || *options.I && strings.ToLower(line) != strings.ToLower(lines[i]) {
+		lineToCompare := line
+		if *options.I {
+			lineToCompare = strings.ToLower(lineToCompare)
+			lines[i] = strings.ToLower(lines[i])
+		}
+		if *options.F != 0 {
+			lineToCompareFields := strings.Fields(lineToCompare)
+			linesIFields := strings.Fields(lines[i])
+
+			if *options.F > len(lineToCompareFields) {
+				lineToCompare = ""
+			} else {
+				lineToCompare = strings.Join(lineToCompareFields[*options.F:], " ")
+			}
+
+			if *options.F > len(linesIFields) {
+				lines[i] = ""
+			} else {
+				lines[i] = strings.Join(linesIFields[*options.F:], " ")
+			}
+		}
+
+		if lineToCompare != lines[i] {
 			if *options.C {
 				uniqueLines[len(uniqueLines)-1] = strconv.Itoa(count) + " " + uniqueLines[len(uniqueLines)-1]
 			} else if *options.D {
